a76c8a93429970e5f5a18df73aafe2caf5a07b74
Ahantejaswi <101503556+Ahantejaswi@users.noreply.github.com>
2024-08-13 20:56:29 +0530

    Update ds.c

--- ds.c
+++ ds.c
@@ -14,3 +14,22 @@ len=strlen(data);
 for(i=0;i<len;i++)
 {
 for(j=0;j<36 && !present;++j)
+{
+if(seq[j]==tolower(data[i]))
+{
+if(isupper(data[i]))
+decoded[i]='a'+j;
+elseif(islower(data[i]))
+decoded[i]='A'+j;
+else decoded[i]='0'+(j-26);
+present=1;
+}
+}
+if(!present)
+decoded[i]=data[i];
+else
+present=0;
+}
+decoded[len]='\0';
+printf("\n Decoded string is:%s", decoded);
+}
